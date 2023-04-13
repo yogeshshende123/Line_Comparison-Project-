72e05de3f21242c4a426baa890e5699b4f4cc14d
yogeshshende123 <yogeshshende1109@gmail.com>
2023-04-13 21:40:37 +0530

    Added a code for lenght of line

--- src/main/java/LineComparisonProject/LineComparion.java
+++ src/main/java/LineComparisonProject/LineComparion.java
@@ -5,5 +5,18 @@ import java.util.Scanner;
 public class LineComparion {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program On Master Branch");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the point x1 :");
+         int x1 = sc.nextInt();
+        System.out.println("Enter the point x2 :");
+            int x2 = sc.nextInt();
+        System.out.println("Enter the point y1 :");
+            int y1 = sc.nextInt();
+        System.out.println("Enter the point y2 :");
+            int y2 = sc.nextInt();
+        // UC1_calculate the lenght of line (x,y) in coordinate cartesian system.
+        double line1 = ((x2-x1)^2+(y2-y1)^2);
+        System.out.println("Lenght of line1 is :" +Math.sqrt(line1));
+
     }
 }
