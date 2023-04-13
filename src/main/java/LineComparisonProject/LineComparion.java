f402b411cdc86d3a25bf59e359dee98de47116ac
yogeshshende123 <yogeshshende1109@gmail.com>
2023-04-13 21:19:20 +0530

    Added welcome message on master branch

---
+++ src/main/java/LineComparisonProject/LineComparion.java
@@ -0,0 +1,9 @@
+package LineComparisonProject;
+
+import java.util.Scanner;
+
+public class LineComparion {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program On Master Branch");
+    }
+}
